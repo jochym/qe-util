8d078fb6b5f40af40a1878375cc9967664a563d2
Paweł T. Jochym <Pawel.Jochym@ifj.edu.pl>
2014-01-02 18:57:11 -0600

    Working energy and stress calculator. Rudimentary remote queue support. Needs some improvement but works.

--- qeutil/__init__.py
+++ qeutil/__init__.py
@@ -7,23 +7,26 @@ http://www.quantum-espresso.org/
 
 
 
-from ase import Atom, Atoms
 import os
 from glob import glob
 from os.path import join, isfile, islink
 
 import numpy as np
 
-from ase.data import atomic_numbers
+import ase.units
 from ase.units import Bohr, Hartree, Rydberg, fs
-from ase.data import chemical_symbols
-from ase.calculators.calculator import FileIOCalculator, Parameters, kpts2mp, ReadError
+from ase import Atom, Atoms
+from ase.data import atomic_numbers, chemical_symbols
+from ase.calculators.calculator import FileIOCalculator, Parameters, kpts2mp, ReadError, all_changes
 
 # Doubtful practice should be more selective in future
 from readers import *
 from writers import *
 from qeio import *
 from analyzers import *
+
+class CalcNotReadyError(Exception):
+    pass
 
 
 class QuantumEspresso(FileIOCalculator):
@@ -36,20 +39,90 @@ class QuantumEspresso(FileIOCalculator):
 
     implemented_properties = ['energy', 'forces', 'stress']
 
+    default_parameters = {
+                'calc':'scf',
+                'outdir': 'tmp',
+                'asr':'crystal',
+                'pseudo_dir': '../pspot',
+                'ndos': 200,
+                'points': 100,
+                'tstress': True,
+                'ecutwfc': 50,
+                'ibrav': 0,
+                'pp_type': 'nc',
+                'pp_format': 'ncpp'
+    }
+    'Default parameters'
+
     def __init__(self,label=None,atoms=None,
-                    pw_cmd='pw.x',
-                    ph_cmd='ph.x',
+                    pw_cmd='pw.x <pw.in >pw.out',
+                    ph_cmd='ph.x <ph.in >ph.out',
                     matdyn_cmd='matdyn.x',
                     q2r_cmd='q2r.x',
+                    wdir='./',
                     **kwargs):
         FileIOCalculator.__init__(self,label=label,atoms=atoms,command=None,**kwargs)
-        self.atoms=atoms.copy()
         self.label=label
+        self.prefix=label
         self.pw_cmd=pw_cmd
         self.ph_cmd=ph_cmd
         self.matdyn_cmd=matdyn_cmd
         self.q2r_cmd=q2r_cmd
+        self.directory=make_calc_dir(self.prefix,wdir)
+        self.submited=False
+        print self.directory
 
+    def calculate(self, atoms=None, properties=['energy'],
+                  system_changes=all_changes):
+        if self.submited :
+            raise CalcNotReadyError
+        if {'energy','stress'} and set(properties) :
+            self.command=self.pw_cmd
+            try :
+                FileIOCalculator.calculate(self, atoms, 
+                    list({'energy','stress'} and set(properties)),
+                    system_changes)
+            except CalcNotReadyError :
+                self.submited=True
+            
 
+    def write_input(self, atoms, properties=None, system_changes=None):
+        """Write input file(s).
+            
+        """
+        self.set(prefix=self.prefix)
+        self.atoms2params()
+        FileIOCalculator.write_input(self, atoms, properties, system_changes)
+        write_pw_in(self.directory, self.atoms, self.parameters)
+
+    def read_results(self):
+        """Read energy, forces, ... from output file(s)."""
+        
+        fn=os.path.join(self.directory,'pw.out')
+        # Read the pan-ultimate line of the output file
+        try: 
+            ln=open(fn).readlines()[-2]
+            if ln.find('JOB DONE.')>-1 :
+                # Job is done we can read the output
+                r=read_quantumespresso_textoutput(fn)
+                self.results['energy']=r['etotal']
+                s=array(r['stress'])* 1e-1 * ase.units.GPa
+                self.results['stress']=array([s[0, 0], s[1, 1], s[2, 2],
+                                       s[1, 2], s[0, 2], s[0, 1]])
+                self.submited=False
+            else :
+                # Job not ready.
+                raise CalcNotReadyError
+        except IOError :
+            # Job not ready.
+            raise CalcNotReadyError
+        
+    def atoms2params(self):
+        '''
+            Populate parameters dictionary with data extracted from atoms.
+            Return the updated dictionary.
+        '''
+        p=self.parameters
+        return p
 
 
--- qeutil/writers.py
+++ qeutil/writers.py
@@ -1,4 +1,5 @@
 from tempfile import mkdtemp
+import os
 
 # PWscf input file
 pw_in='''
@@ -10,7 +11,6 @@ pw_in='''
   outdir = './%(outdir)s/',
 /
 &SYSTEM
-    A = %(A)f,
     ecutwfc = %(ecut)f,
     ibrav = %(ibrav)d,
     nat = %(nat)d,
@@ -69,7 +69,103 @@ phdos_in='''
  /
 '''
 
-def make_calc_dir(bdir,params):
+def write_section_params(fh, k, p):
+    """ Automatically writes all the specified parameters for this section.
+        Multidimensional parameters are taken into account properly.
+    """
+    for key, value in p.items():
+        # Nothing set skip
+        if value is None:
+            continue
+        # Not this section skip
+        if not key in k :
+            continue
+
+        # Write the rest
+        if isinstance(value, list):
+            if inp_writeall[key] == 1:
+                for element in value:
+                    fh.write('    %s(%d) = %s' % (key, value.index(element), str(value)))
+            else:
+                for i in range(len(value)):
+                    if inp_writeall[key][i] == 1:
+                        # i+1 instead of i because enumeration must start from 1
+                        fh.write('    %s(%d) = %s,\n' % (key, i+1, str(value[i])))   
+
+        elif isinstance(value, bool):
+            fh.write('    %s = .%s.,\n' % (key, str(value).lower()) )
+        elif isinstance(value, float):
+            fh.write('    %s = %g,\n' % (key, value))
+        elif isinstance(value, int):
+            fh.write('    %s = %d,\n' % (key, value))
+        elif isinstance(value, basestring):  
+            fh.write("    %s = '%s',\n" % (key, value))
+
+
+def write_pw_in(d,a,p):
+
+    fh=open(os.path.join(d,'pw.in'),'w')
+    # ----------------------------------------------------------
+    # | CONTROL section
+    # ----------------------------------------------------------
+
+    fh.write(' &CONTROL\n')
+    fh.write("    calculation = '%s',\n" % p['calc'])
+
+    pwin_k=['tstress','nstep','pseudo_dir','outdir']
+    write_section_params(fh, pwin_k, p)
+    fh.write(' /\n')
+    
+    # ----------------------------------------------------------
+    # | SYSTEM section
+    # ----------------------------------------------------------
+
+    pwin_k=['ecutwfc','ibrav']
+    fh.write(' &SYSTEM\n')
+    write_section_params(fh, pwin_k, p)
+    fh.write("    nat = %d\n" % (a.get_number_of_atoms()))
+    fh.write("    ntyp = %d\n" % (len(set(a.get_atomic_numbers()))))
+    fh.write(' /\n')
+
+    # ----------------------------------------------------------
+    # | ELECTRONS section
+    # ----------------------------------------------------------
+
+    fh.write(' &ELECTRONS\n')
+    fh.write(' /\n')
+
+    
+    # ----------------------------------------------------------
+    # | Card: ATOMIC_SPECIES
+    # ----------------------------------------------------------
+        
+    fh.write('ATOMIC_SPECIES\n')
+    
+    xc=p['xc']
+    pp_type=p['pp_type']
+    pp_format=p['pp_format']
+    for nm, mass in set(zip(a.get_chemical_symbols(),a.get_masses())):
+        fh.write("    %s %g %s_%s_%s.%s \n" % (nm, mass, nm, xc, pp_type, pp_format))
+
+    # ----------------------------------------------------------
+    # | Card: ATOMIC_POSITIONS
+    # ----------------------------------------------------------
+    
+    fh.write('ATOMIC_POSITIONS {crystal}\n')
+    for n,v in zip(a.get_chemical_symbols(),a.get_scaled_positions()):
+        fh.write("    %s %g %g %g\n" % (n, v[0], v[1], v[2]))
+
+    # ----------------------------------------------------------
+    # | Card: CELL_PARAMETERS
+    # ----------------------------------------------------------
+    
+    fh.write('CELL_PARAMETERS {angstrom}\n')
+    
+    for v in a.get_cell():
+        fh.write('    %f %f %f\n' % tuple(v))
+    
+
+def make_calc_dir(prefix, bdir='./'):
     '''
     Builds a calculation directory in the given base directory.
     Creates a unique calculation directory and builds all ".in" 
@@ -84,8 +180,8 @@ def make_calc_dir(bdir,params):
     
     INPUT
     -----
-        bdir - base directory
         params - calculation parameters dictionary
+        bdir - base directory
     
     OUTPUT
     ------
@@ -93,21 +189,20 @@ def make_calc_dir(bdir,params):
     
     '''
     
-    bcdir=mkdtemp(prefix=params['prefix']+'.',dir=bdir)
-    cdir=os.path.split(bcdir)[1]
+    bcdir=mkdtemp(prefix=prefix + '.',dir=bdir)
+    #cdir=os.path.split(bcdir)[1]
     #print cdir, bcdir
     
-    open(os.path.join(bcdir,'pw.in'),'w').write(pw_in % params)
-    open(os.path.join(bcdir,'ph.in'),'w').write(ph_in % params)
-    open(os.path.join(bcdir,'q2r.in'),'w').write(q2r_in % params)
-    f=open(os.path.join(bcdir,'matdyn.in'),'w')
-    f.write(matdyn_in % params)
-    f.write('%d\n'%qp.shape[0])
-    for v in qp:
-        f.write("%f %f %f %d\n" % (v[0],v[1],v[2],params['points']))
-    f.close()
-    open(os.path.join(bcdir,'phdos.in'),'w').write(phdos_in % params)
-    return bcdir,cdir
+#    open(os.path.join(bcdir,'ph.in'),'w').write(ph_in % params)
+#    open(os.path.join(bcdir,'q2r.in'),'w').write(q2r_in % params)
+#    f=open(os.path.join(bcdir,'matdyn.in'),'w')
+#    f.write(matdyn_in % params)
+#    f.write('%d\n'%qp.shape[0])
+#    for v in qp:
+#        f.write("%f %f %f %d\n" % (v[0],v[1],v[2],params['points']))
+#    f.close()
+#    open(os.path.join(bcdir,'phdos.in'),'w').write(phdos_in % params)
+    return bcdir
 
 def make_D3_files(bdir,clc,cdat):
     open(os.path.join(bdir,clc,'phD3G.in'),'w').write(ph3G_in % cdat)
