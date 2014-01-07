0f508295af51ad44532365c1f11863d1c0ddd7dd
Paweł T. Jochym <pawel.jochym@ifj.edu.pl>
2014-01-07 14:15:31 -0600

    Bump the version to 0.1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='qeutil',
-    version='0.1.5',
+    version='0.1.6',
     packages=['qeutil',],
     license='GPLv3',
     description = 'A set of utilities for Quantum Espresso',
