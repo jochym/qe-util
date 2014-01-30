ab14004afd55dcb2b04800350f5176e60ebac7ff
Paweł T. Jochym <jochym@users.noreply.github.com>
2014-01-30 17:15:07 -0600

    Update analyzers.py

--- qeutil/analyzers.py
+++ qeutil/analyzers.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#    Copyright 1998-2014 by Paweł T. Jochym <pawel.jochym@ifj.edu.pl>
+#
+#    qe-util is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    qe-util is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with QE-util.  If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import division
 from numpy import array
 import numpy
