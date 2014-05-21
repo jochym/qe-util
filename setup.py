4c52530f371fd9e61182fd885ae76d0c9f193a4f
Paweł T. Jochym <pawel.jochym@ifj.edu.pl>
2014-05-22 01:25:16 +0200

    Added dependencies to setup.py.

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ from distutils.core import setup
 
 setup(
     name='qeutil',
-    version='0.1.15',
-    packages=['qeutil',],
+    version='0.1.16',
+    packages=['qeutil'],
     license='GPLv3',
     description = 'A set of utilities for Quantum Espresso',
     author = 'Paweł T. Jochym',
     author_email = 'Pawel.Jochym@ifj.edu.pl',
-    url = 'https://github.com/jochym/qe-util',   # use the URL to the github repo
-    download_url = 'https://github.com/jochym/qe-util/tarball/0.1.10', # I'll explain this in a second
-    keywords = ['science', 'physics', 'quantum-espresso'], # arbitrary keywords
+    url = 'https://github.com/jochym/qe-util',   
+    download_url = 'https://github.com/jochym/qe-util/', 
+    keywords = ['science', 'physics', 'quantum-espresso'], 
+    requires = ['ase','numpy','scipy','pyspglib'],
     classifiers = [],
 )
